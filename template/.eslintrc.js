14f834cd9f6e70d76c7cea807148f3b13f259bff
zhangbobo [张波波] <zhangbobo@xiaomi.com>
2017-04-27 17:31:07 +0800

    changed eslint && indent to 4

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,55 +1,62 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  {{#if_eq lintConfig "standard"}}
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  {{/if_eq}}
-  {{#if_eq lintConfig "airbnb"}}
-  extends: 'airbnb-base',
-  {{/if_eq}}
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  {{#if_eq lintConfig "airbnb"}}
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
-    }
-  },
-  {{/if_eq}}
-  // add your custom rules here
-  'rules': {
-    {{#if_eq lintConfig "standard"}}
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    {{/if_eq}}
-    {{#if_eq lintConfig "airbnb"}}
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
-    {{/if_eq}}
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
-}
+    'root': true,
+    'parser': 'babel-eslint',
+    'parserOptions': {
+        'sourceType': 'module'
+    },
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es6': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'sourceType': 'module'
+    },
+    'rules': {
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'curly': [
+            'error',
+            'all'
+        ],
+        'array-bracket-spacing': [
+            'error',
+            'always'
+        ],
+        'block-spacing': [
+            'error',
+            'always'
+        ],
+        'brace-style': [
+            'error',
+            '1tbs'
+        ],
+        'comma-spacing': [
+            'error',
+            { 
+                'before': false, 
+                'after': true 
+            }
+        ]
+    },
+    'plugins': [
+        'html'
+    ]
+};
